0069e288047b9d7048c04f36fbbf8373b4645c71
loryconti <loryconti1@gmail.com>
2024-04-01 00:35:21 +0200

    Completely removed members & admins fetch in fetchbyuser.php

--- PHP/list/fetchbyuser.php
+++ PHP/list/fetchbyuser.php
@@ -10,7 +10,6 @@
         $db = new Database();
         $conn = $db->get_connection();
         if($conn === false){
-            echo "a-1";
             $errorCode = sqlsrv_errors()[0]['code'];
             die("Error: " . $errorCode);
         }
@@ -18,46 +17,13 @@
         $tsql = "select * from [dbo].[lists] where id in (select distinct listid from [dbo].[listmembers] where userid = ?)";
         $stmt = sqlsrv_query($conn, $tsql, array($userid));
         if ($stmt === false){
-            echo "a0";
             $errorCode = sqlsrv_errors()[0]['code'];
             die("Error: " . $errorCode);
             return;
         }
-
-        
-        
         
         // fetch
         while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){            
-            // fetch members
-            $tsql = "select userid from [dbo].[listmembers] where listid = ?";
-            $stmt2 = sqlsrv_query($conn, $tsql, array($row['id']));
-            if ($stmt2 === false){
-                $errorCode = sqlsrv_errors()[0]['code'];
-                die("Error: " . $errorCode);
-                return;
-            }
-            $members = array();
-            while($row2 = sqlsrv_fetch_array($stmt2, SQLSRV_FETCH_ASSOC)){
-                array_push($members, $row2['userid']);
-            }
-
-            // fetch admins
-            $tsql = "select userid from [dbo].[listadmins] where listid = ?";
-            $stmt2 = sqlsrv_query($conn, $tsql, array($row['id']));
-            if ($stmt2 === false){
-                $errorCode = sqlsrv_errors()[0]['code'];
-                die("Error: " . $errorCode);
-                return;
-            }
-            $admins = array();
-            while($row2 = sqlsrv_fetch_array($stmt2, SQLSRV_FETCH_ASSOC)){
-                array_push($admins, $row2['userid']);
-            }
-
-            // free statements
-            sqlsrv_free_stmt($stmt2);
-
             $l = new MyList(
                 $row['id'],
                 $row['name'],
