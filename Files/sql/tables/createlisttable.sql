d3cf24f22df0d18ba15f621035bbc9d7aba3f54e
loryconti <loryconti1@gmail.com>
2024-03-31 23:56:37 +0200

    Added new list table attributes

--- Files/sql/tables/createlisttable.sql
+++ Files/sql/tables/createlisttable.sql
@@ -1,7 +1,9 @@
 create table lists(
 id int primary key not null,
 name varchar(80) not null,
+description varchar(255) not null,
 creatorid int not null,
+color int not null,
 code varchar(80) not null,
 lastupdated datetime not null,
 creationdate datetime not null
