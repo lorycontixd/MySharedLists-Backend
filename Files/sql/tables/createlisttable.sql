5cd736ae89f0a6cba37eb0d9ee5f96e55d5d3214
loryconti <loryconti1@gmail.com>
2024-04-01 16:53:39 +0200

    Added icon id to createlisttable.sql

--- Files/sql/tables/createlisttable.sql
+++ Files/sql/tables/createlisttable.sql
@@ -4,6 +4,7 @@ name varchar(80) not null,
 description varchar(255) not null,
 creatorid int not null,
 color int not null,
+iconid int not null,
 code varchar(80) not null,
 lastupdated datetime not null,
 creationdate datetime not null
