a2af923298bf7195d22fe43b88e745071a11c271
loryconti <loryconti1@gmail.com>
2024-04-13 13:11:33 +0200

    Set debugmode to false in register.php

--- PHP/user/register.php
+++ PHP/user/register.php
@@ -132,6 +132,7 @@
             $userrow['password'],
             $userrow['firstname'],
             $userrow['lastname'],
+            $userrow['iconurl'],
             $userrow['subscriptionplan'],
             $userrow['subscriptiondate'],
             $userrow['subscriptionenddate'],
